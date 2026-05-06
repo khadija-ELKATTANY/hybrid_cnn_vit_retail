b2ac5be0d62b396dc4c37117895294b3bc728eb6
khadija-ELKATTANY <khadijaelkattany94@gmail.com>
2026-05-06 10:27:02 +0000

    Update encoder and training script with correlation-aware logic

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+class Config:
+    DATA_PATH = "data/raw/retail_dataset.csv"   # change if needed
+    IMAGE_SIZE = 32
+    BATCH_SIZE = 64
+    NUM_EPOCHS = 50
+    LEARNING_RATE = 1e-3
+    NUM_CLASSES = 8
